688eb67cd14f63ccf1d8dc74d7c662eb63ac6447
Taufiq Hasbi <taufiq.hasbi@ralali.com>
2016-10-28 14:43:48 +0700

    add about link

--- test/ping.spec.js
+++ test/ping.spec.js
@@ -7,27 +7,27 @@ var server  = require('../libs/server');
 describe('Passing the ping level:', function() {
 
     var testServer;
-    
+
     beforeEach(function(done) {
         testServer = http.createServer(server).listen(7000, done);
     });
 
     afterEach(function() {
-        testServer.close(); 
+        testServer.close();
     });
-   
+
     it('answers with application/json header', function(done) {
         request('http://localhost:7000/ping', function(error, response, body) {
             expect(response.headers['content-type']).to.equal('application/json');
-            done(); 
+            done();
         });
     });
-   
+
     it('returns the expected pong', function(done) {
         request('http://localhost:7000/ping', function(error, response, body) {
             expect(body.replace(/\r?\n|\r|\s/g,"")).to.equal( JSON.stringify({"alive":true}) );
-            done(); 
+            done();
         });
     });
-   
+
 });

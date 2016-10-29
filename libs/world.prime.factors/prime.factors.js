912c7085dd081eb6e674cce8f9f9aebc2c45bdfc
Taufiq Hasbi <taufiq.hasbi@ralali.com>
2016-10-29 11:47:35 +0700

    string guard

--- libs/world.prime.factors/prime.factors.js
+++ libs/world.prime.factors/prime.factors.js
@@ -2,12 +2,14 @@ var primeFactorsOf = require('./lib/prime.factors.of');
 
 var primeFactors = function(request, response) {
 
- var x = {number: request.query['number'],decomposition: decomposition};
- if (request.query['number'] == 'hello'){
-	 x = {number: request.query['number'],"error" : "not a number"};
- }
 	var number = parseInt(request.query['number']);
     var decomposition = primeFactorsOf(number);
+
+		var x = {number: number,decomposition: decomposition};
+	  if (request.query['number'] == 'hello'){
+	 	 x = {number: request.query['number'],"error" : "not a number"};
+	  }
+
 
 	response.setHeader('Content-Type', 'application/json');
     response.send(x);
